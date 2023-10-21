064181ad52c9fd3c737185b9c0b89809f3a1bd42
FDS <fds@brunhild.cn>
2023-10-21 20:15:14 +0800

    update

--- 3. Firmware/circuller-firmware.ino
+++ 3. Firmware/circuller-firmware.ino
@@ -1,3 +1,16 @@
+#include "Keyboard.h"
+#include "FastLED.h"
+
+// Define the button pins
+#define BUTTON_W_PIN 3
+#define BUTTON_E_PIN 4
+#define BUTTON_D_PIN 5
+#define BUTTON_C_PIN 6
+#define BUTTON_X_PIN 7
+#define BUTTON_Z_PIN 8
+#define BUTTON_A_PIN 9
+#define BUTTON_Q_PIN 10
+
 #if defined(__AVR_ATmega32U4__) || defined(ARDUINO_SAMD_ZERO)
 #pragma message "当前的开发板是 ATmega32U4 或 SAMD_ZERO"
 #define SerialDevice SerialUSB
@@ -13,18 +26,16 @@
 #define SerialDevice Serial
 #define LED_PIN 13
 
-
 #else
 #error "未经测试的开发板，请检查串口和阵脚定义"
 #endif
 
-#include "FastLED.h"
 #define NUM_LEDS 11
 CRGBArray<NUM_LEDS> leds;
 
 // Define the LED order
 int ledOrder[8] = {3, 2, 1, 0, 7, 6, 5, 4};
-
+//int ledOrder[8] = {0, 1, 2, 3, 4, 5, 6, 7};
 enum {
   LedGs8Bit = 0x31,//49
   LedGs8BitMulti = 0x32,//50
@@ -103,12 +114,25 @@ static uint8_t packet_read() {
 }
 
 void setup() {
+  // Initialize buttons as input with internal pullup resistor
+  pinMode(BUTTON_W_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_E_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_D_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_C_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_X_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_Z_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_A_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_Q_PIN, INPUT_PULLUP);
+
+  // Begin keyboard emulation
+  Keyboard.begin();
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
   FastLED.setBrightness(30);
   FastLED.clear();
   SerialDevice.begin(115200);
-  leds(ledOrder[req.index]) = 0xFFFFFF;
+  leds[ledOrder[req.index]] = 0xFFFFFF;
   FastLED.delay(1000);
+
 }
 
 unsigned long fade_start, fade_end, progress;
@@ -116,6 +140,19 @@ uint8_t led_start, led_end, fade_tag;
 CRGB fade_prev, fade_taget;
 
 void loop() {
+  // If a button is pressed (LOW), emulate the corresponding key press
+  if(digitalRead(BUTTON_W_PIN) == LOW) Keyboard.write('w');
+  if(digitalRead(BUTTON_E_PIN) == LOW) Keyboard.write('e');
+  if(digitalRead(BUTTON_D_PIN) == LOW) Keyboard.write('d');
+  if(digitalRead(BUTTON_C_PIN) == LOW) Keyboard.write('c');
+  if(digitalRead(BUTTON_X_PIN) == LOW) Keyboard.write('x');
+  if(digitalRead(BUTTON_Z_PIN) == LOW) Keyboard.write('z');
+  if(digitalRead(BUTTON_A_PIN) == LOW) Keyboard.write('a');
+  if(digitalRead(BUTTON_Q_PIN) == LOW) Keyboard.write('q');
+
+  // Add a small delay to prevent button bouncing
+  delay(65);
+
   switch (packet_read()) {
     case LedGs8Bit:
       leds[req.index] = CRGB(req.r, req.g, req.b);
@@ -164,4 +201,5 @@ void loop() {
       leds[ledOrder[i]] = blend(fade_prev, fade_taget, progress);
   }
   FastLED.show();
+
 }
